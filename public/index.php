520d6284915ff00689f8c5a21bfec63525c4ee6d
AttilaJenei <work@attilajenei.com>
2016-05-21 15:47:03 +0200

    Scope encapsulate

--- public/index.php
+++ public/index.php
@@ -8,30 +8,33 @@ $phpStartMem = memory_get_usage();
 $phpStartMemPeak = memory_get_peak_usage();
 $phpStartTime = microtime(true);
 
-// Check host is allowed
-$hosts = ['www.fixin-dev.attila'];
-$requestedHost = $_SERVER['SERVER_NAME'];
+(function() {
+/*
+    // Check host is allowed
+    $hosts = ['www.fixin-dev.attila'];
+    $requestedHost = $_SERVER['SERVER_NAME'];
 
-if (!in_array($requestedHost, $hosts)) {
-    header("HTTP/1.1 404 Not Found");
-    echo '<!DOCTYPE html><html><body><h1>Not Found</h1></body></html>';
+    if (!in_array($requestedHost, $hosts)) {
+        header("HTTP/1.1 404 Not Found");
+        echo '<!DOCTYPE html><html><body><h1>Not Found</h1></body></html>';
 
-    exit;
-}
+        exit;
+    }
 
-// Load config
-$topPath = dirname(__DIR__);
-$config = require "{$topPath}/config/{$requestedHost}.php";
+    // Load config
+    $topPath = dirname(__DIR__);
+    $config = require "{$topPath}/config/{$requestedHost}.php";
 
-// Autoloader
-require "{$fixinPath}/classes/Fixin/Base/Autoloader/SimpleAutoloader.php";
-$autoloader = new \Fixin\Base\Autoloader\SimpleAutoloader($config['loader']['prefixes']);
-$autoloader->register();
+    // Autoloader
+    require "{$fixinPath}/classes/Fixin/Base/Autoloader/SimpleAutoloader.php";
+    $autoloader = new \Fixin\Base\Autoloader\SimpleAutoloader($config['loader']['prefixes']);
+    $autoloader->register();
+*/
+    $application = include '../../Fixin/cheats/web.php';
+    // Application
+//     $application = new \Fixin\Application\Application($config);
+    $application->run();
 
-// $application = include '../../Fixin/cheats/web.php';
-// Application
-$application = new \Fixin\Application\Application($config);
-$application->run();
+})();
 
-
-echo 'Run: ' . number_format((microtime(true) - $phpStartTime) * 1000, 4) . 'ms, ' . (memory_get_usage() - $phpStartMem) . ' / ' . (memory_get_peak_usage() - $phpStartMemPeak) . ' bytes';
+echo 'Run: ' . number_format((microtime(true) - $phpStartTime) * 1000, 4) . 'ms, ' . (memory_get_usage() - $phpStartMem) . ' / ' . (memory_get_peak_usage() - $phpStartMemPeak) . ' bytes', '<br />';
