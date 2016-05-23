6c5e93f00dd91a548bf0fb7889f0723b14c2cff2
AttilaJenei <work@attilajenei.com>
2016-05-23 17:05:32 +0200

    File loading order

--- public/index.php
+++ public/index.php
@@ -36,4 +36,46 @@ $phpStartTime = microtime(true);
 
 })();
 
-echo 'Run: ' . number_format((microtime(true) - $phpStartTime) * 1000, 4) . 'ms, ' . (memory_get_usage() - $phpStartMem) . ' / ' . (memory_get_peak_usage() - $phpStartMemPeak) . ' bytes', '<br />';
+echo '<pre>---<br />';
+echo 'Execution time:   ' . number_format((microtime(true) - $phpStartTime) * 1000, 4) . 'ms<br />';
+echo 'Memory usage:     ' . (memory_get_usage() - $phpStartMem) . ' / ' . (memory_get_peak_usage() - $phpStartMemPeak) . ' bytes', '<br />';
+
+$includedFiles = get_included_files();
+
+// $prefix = dirname(reset($includedFiles));
+// $length = mb_strlen($prefix);
+$last = '';
+$list = [];
+
+foreach ($includedFiles as $file) {
+    $path = dirname($file) . '/';
+    $length = mb_strlen($path);
+    $padding = false;
+
+    while ($path !== mb_substr($last, 0, $length)) {
+        $path = dirname($path) . '/';
+        $diff = $length - mb_strlen($path);
+
+        if ($diff) {
+            $length -= $diff;
+            $padding = true;
+            continue;
+        }
+
+        $length = 0;
+        break;
+    }
+
+    if ($padding) {
+        $list[] = '';
+    }
+
+    $list[] = str_repeat(' ', $length) . mb_substr($file, $length);
+    $last = $file;
+}
+
+// $length++;
+
+echo 'Included files:   ' . count($includedFiles) . '<br />                  ';
+echo implode("<br />                  ", $list);
+echo '</pre>';
