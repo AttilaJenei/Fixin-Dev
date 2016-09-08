3e3196d027c8347944bdc45be97c22e6703352c5
AttilaJenei <work@attilajenei.com>
2016-09-08 16:56:25 +0200

    Complex queries

--- classes/App/Controller/Index.php
+++ classes/App/Controller/Index.php
@@ -6,15 +6,39 @@
 
 namespace App\Controller;
 
+use App\System\User\Repository as UserRepository;
 use Fixin\Controller\ActionController;
 use Fixin\Delivery\Cargo\HttpCargoInterface;
-use App\System\User\Repository as UserRepository;
 
 class Index extends ActionController {
 
     public function indexAction(HttpCargoInterface $cargo): HttpCargoInterface {
         /** @var $repository UserRepository */
-        $repository = $this->container->get('System\User\Repository');
+        $userRepository = $this->container->get('System\User\Repository');
+        $groupRepository = $this->container->get('System\Group\Repository');
+
+        $request = $userRepository->createRequest()
+        ->setAlias('proba');
+
+        $request->getWhere()
+        ->notNull('proba.email')
+        ->compare('text', '<=', 'abcd');
+
+        $request
+        ->join($groupRepository, 'group.groupID', '=', 'proba.groupID-1', 'group')
+        ->setColumns([
+            'date',
+            'string' => 'ezaz',
+            'max' => $request->createExpression('MAX(userID)'),
+            'min' => $request->createExpression('MIN(userID / ?)', [2])
+        ])
+        ->setGroupBy(['test', $request->createExpression('test2')])
+        ->setOrderBy(['test' => 'asc', 'kettes'])
+        ->setLimit(20)
+        ;
+//         $request->fetchRawData();
+        $request->insertInto($groupRepository);
+        /*
         $repository->create()
         ->setName('Test')
         ->setEmail('test@example.com')
@@ -22,7 +46,7 @@ class Index extends ActionController {
 //         $repository->
 //         $repository->create
 //         $entity = $repository->createId(1)->getEntity();
-
+*/
         return $cargo;
     }
 }
